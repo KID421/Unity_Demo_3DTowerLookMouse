80660ee7e5e851dfcbde3454a6db3253cd7f273a
KID <50666207+KID421@users.noreply.github.com>
2020-10-12 21:30:34 +0800

    完成

---
+++ TowerLookMouse/Assets/LookMouse.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class LookMouse : MonoBehaviour
+{
+    private void Update()
+    {
+        Look();
+    }
+
+    private void Look()
+    {
+        Vector3 posMouse = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10);
+        Vector3 posWorld = Camera.main.ScreenToWorldPoint(posMouse);
+
+        posWorld.y = transform.position.y;
+
+        Vector3 direction = posWorld - transform.position;
+
+        transform.forward = direction;
+    }
+}
